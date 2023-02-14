e6c2caeebd8c85ec9efe3a21a75796694f07e1d6
AliHabesh <ali.habesh0068@stud.hkr.se>
2023-02-14 09:12:23 +0100

    added equipment map and ArmorAttributes

--- src/equipments/Armor.java
+++ src/equipments/Armor.java
@@ -1,5 +1,8 @@
 package equipments;
 
+import heroes.HeroAttributes;
+
 public class Armor {
     private ArmorType armorType;
+    private HeroAttributes armorAttribute;
 }
--- src/heroes/Hero.java
+++ src/heroes/Hero.java
@@ -1,5 +1,10 @@
 package heroes;
 
+import equipments.Item;
+import equipments.Slot;
+
+import java.util.HashMap;
+
 /**
  * Abstract class which is the foundation class for all the heroes
  */
@@ -8,9 +13,21 @@ public abstract class Hero {
     private int level;
     private HeroAttributes heroAttributes;
 
+    private HashMap<Slot, Item> equipment;
 
     public Hero(String name){
         this.name = name;
+        initializeEquipment();
+    }
+
+    /**
+     Initializes equipments with null values for each Slot enum value.
+     **/
+    private void initializeEquipment(){
+        this.equipment.put(Slot.Head, null);
+        this.equipment.put(Slot.Body, null);
+        this.equipment.put(Slot.Legs, null);
+        this.equipment.put(Slot.Weapon, null);
     }
 
     public abstract void LevelUp();
